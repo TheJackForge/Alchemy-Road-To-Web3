b829897a89e84c4b334a903fff76b224a0eed015
Jack Forge <kimfidler@gmail.com>
2022-07-16 09:25:10 -0600

    Hardhat Contracts Completed

--- Week 002/contracts/BuyMeACoffee.sol
+++ Week 002/contracts/BuyMeACoffee.sol
@@ -4,7 +4,7 @@
 pragma solidity ^0.8.0;
 
 // Switch this to your own contract address once deployed, for bookkeeping!
-// Example Contract Address on Goerli: 0xDBa03676a2fBb6711CB652beF5B7416A53c1421D
+// Example Contract Address on Goerli: 0x5FbDB2315678afecb367f032d93F642f64180aa3
 
 contract BuyMeACoffee {
     // Event to emit when a Memo is created.
---
+++ Week 002/scripts/withdraw.js
@@ -0,0 +1,49 @@
+// scripts/withdraw.js
+
+const hre = require("hardhat");
+const abi = require("../artifacts/contracts/BuyMeACoffee.sol/BuyMeACoffee.json");
+
+async function getBalance(provider, address) {
+    const balanceBigInt = await provider.getBalance(address);
+    return hre.ethers.utils.formatEther(balanceBigInt);
+}
+
+async function main() {
+    // Get the contract that has been deployed to Goerli
+    const contractAddress = "0xb0f704aCb296CD169f107690d6e298aDD63a0252";
+    const contractABI = abi.abi;
+
+    // Get the node connection and wallet connection
+    const provider = new hre.ethers.providers.AlchemyProvider("goerli", process.env.GOERLI_API_KEY);
+
+    // Ensure that the signer is the SAME address as the original contract deployer, or else this
+    // script will fail with an error.
+    const signer = new hre.ethers.Wallet(process.env.PRIVATE_KEY, provider);
+
+    // Instantiate the connected contract
+    const buyMeACoffee = new ethers.Contract(contractAddress, contractABI, signer );
+
+    // Check starting balances
+    console.log("current balance of owner: ", await getBalance(provider, signer.address), "ETH");
+    const contractBalance = await getBalance(provider, buyMeACoffee.address);
+    console.log("current balance of contract: ", await getBalance(provider, buyMeACoffee.address), "ETH");
+
+    // Withdraw funds if there are funds to withdraw
+    if (contractBalance !== 0.0) {
+        console.log("Withdrawing Funds...");
+        const withdrawTxn = await buyMeACoffee.withdrawTips();
+        await withdrawTxn.wait();
+    } else {
+        console.log("No funds to withdraw");
+    }
+
+    // Check ending balance
+    console.log("Current balance of owner: ", await getBalance(provider, signer.address), "ETH");
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    })
